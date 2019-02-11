f5f3d74eeec390abf1988d16fa922ea89242661d
Caleb Williams <br0p0p@users.noreply.github.com>
2019-02-11 14:53:48 -0600

    Install Jest, get tests working

--- src/test/async-iterator-subscription.test.ts
+++ src/test/async-iterator-subscription.test.ts
@@ -1,134 +1,129 @@
+import { ConsoleLogger } from '@cdm-logger/server';
+import { isAsyncIterable } from 'iterall';
+import { AmqpPubSub } from '../amqp-pubsub';
+import { parse, GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+import { withFilter } from 'graphql-subscriptions';
+import { subscribe } from 'graphql/subscription';
 
-// import { isAsyncIterable } from 'iterall';
-// import { NatsPubSub } from '../nats-pubsub';
-// import {
-//     parse,
-//     GraphQLSchema,
-//     GraphQLObjectType,
-//     GraphQLString,
-// } from 'graphql';
-// import { withFilter } from 'graphql-subscriptions';
-// import { subscribe } from 'graphql/subscription';
-// import { logger } from './logger';
+const logger = ConsoleLogger.create('integration-test', { level: 'trace' });
 
-// const FIRST_EVENT = 'FIRST_EVENT';
+const FIRST_EVENT = 'FIRST_EVENT';
 
-// const defaultFilter = (payload) => true;
+const defaultFilter = payload => true;
 
-// function buildSchema(iterator, filterFn = defaultFilter) {
-//     return new GraphQLSchema({
-//         query: new GraphQLObjectType({
-//             name: 'Query',
-//             fields: {
-//                 testString: {
-//                     type: GraphQLString,
-//                     resolve: function (_, args) {
-//                         return 'works';
-//                     },
-//                 },
-//             },
-//         }),
-//         subscription: new GraphQLObjectType({
-//             name: 'Subscription',
-//             fields: {
-//                 testSubscription: {
-//                     type: GraphQLString,
-//                     subscribe: withFilter(() => iterator, filterFn),
-//                     resolve: root => {
-//                         return 'FIRST_EVENT';
-//                     },
-//                 },
-//             },
-//         }),
-//     });
-// }
+function buildSchema(iterator, filterFn = defaultFilter) {
+  return new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'Query',
+      fields: {
+        testString: {
+          type: GraphQLString,
+          resolve: function(_, args) {
+            return 'works';
+          }
+        }
+      }
+    }),
+    subscription: new GraphQLObjectType({
+      name: 'Subscription',
+      fields: {
+        testSubscription: {
+          type: GraphQLString,
+          subscribe: withFilter(() => iterator, filterFn),
+          resolve: root => {
+            return 'FIRST_EVENT';
+          }
+        }
+      }
+    })
+  });
+}
 
-// describe('GraphQL-JS asyncIterator', () => {
-//     it('should allow subscriptions', () => {
-//         const query = parse(`
-//         subscription S1 {
-//             testSubscription
-//             }
-//     `);
-//         const pubsub = new NatsPubSub({ logger });
-//         const origIterator = pubsub.asyncIterator(FIRST_EVENT);
-//         const orig2Iterator = pubsub.asyncIterator('TEST123');
+describe('GraphQL-JS asyncIterator', () => {
+  it('should allow subscriptions', () => {
+    const query = parse(`
+        subscription S1 {
+            testSubscription
+            }
+    `);
+    const pubsub = new AmqpPubSub({ logger });
+    const origIterator = pubsub.asyncIterator(FIRST_EVENT);
+    const orig2Iterator = pubsub.asyncIterator('TEST123');
 
-//         const schema = buildSchema(origIterator);
-//         const results = subscribe(schema, query);
-//         const payload1 = results.next();
+    const schema = buildSchema(origIterator);
+    const results = subscribe(schema, query);
+    const payload1 = results.next();
 
-//         expect(isAsyncIterable(results)).toBeTruthy();
+    expect(isAsyncIterable(results)).toBeTruthy();
 
-//         const r = payload1.then(res => {
-//             expect(res.value.data.testSubscription).toEqual('FIRST_EVENT');
-//         });
+    const r = payload1.then(res => {
+      expect(res.value.data.testSubscription).toEqual('FIRST_EVENT');
+    });
 
-//         pubsub.publish(FIRST_EVENT, { test: { file: true } });
+    pubsub.publish(FIRST_EVENT, { test: { file: true } });
 
-//         return r;
-//     });
+    return r;
+  });
 
-//     it('should detect when the payload is done when filtering', (done) => {
-//         const query = parse(`
-//         subscription S1 {
-//             testSubscription
-//         }
-//         `);
+  it('should detect when the payload is done when filtering', done => {
+    const query = parse(`
+        subscription S1 {
+            testSubscription
+        }
+        `);
 
-//         const pubsub = new NatsPubSub({ logger });
-//         const origIterator = pubsub.asyncIterator(FIRST_EVENT);
+    const pubsub = new AmqpPubSub({ logger });
+    const origIterator = pubsub.asyncIterator(FIRST_EVENT);
 
-//         let counter = 0;
+    let counter = 0;
 
-//         const filterFn = () => {
-//             counter++;
+    const filterFn = () => {
+      counter++;
 
-//             if (counter > 10) {
-//                 const e = new Error('Infinite loop detected');
-//                 done(e);
-//                 throw e;
-//             }
-//             return false;
-//         };
+      if (counter > 10) {
+        const e = new Error('Infinite loop detected');
+        done(e);
+        throw e;
+      }
+      return false;
+    };
 
-//         const schema = buildSchema(origIterator, filterFn);
+    const schema = buildSchema(origIterator, filterFn);
 
-//         const results = subscribe(schema, query);
-//         expect(isAsyncIterable(results)).toBeTruthy();
+    const results = subscribe(schema, query);
+    expect(isAsyncIterable(results)).toBeTruthy();
 
-//         results.next();
-//         results.return();
+    results.next();
+    results.return();
 
-//         pubsub.publish(FIRST_EVENT, {});
+    pubsub.publish(FIRST_EVENT, {});
 
-//         setTimeout(_ => {
-//             done();
-//         }, 500);
-//     });
+    setTimeout(_ => {
+      done();
+    }, 500);
+  });
 
-//     it('should clear event handlers', () => {
-//         const query = parse(`
-//         subscription S1 {
-//             testSubscription
-//         }
-//         `);
+  it('should clear event handlers', () => {
+    const query = parse(`
+        subscription S1 {
+            testSubscription
+        }
+        `);
 
-//         const pubsub = new NatsPubSub({ logger });
-//         const origIterator = pubsub.asyncIterator(FIRST_EVENT);
-//         const returnSpy = jest.spyOn(origIterator, 'return');
-//         const schema = buildSchema(origIterator);
+    const pubsub = new AmqpPubSub({ logger });
+    const origIterator = pubsub.asyncIterator(FIRST_EVENT);
+    const returnSpy = jest.spyOn(origIterator, 'return');
+    const schema = buildSchema(origIterator);
 
-//         const results = subscribe(schema, query);
-//         const end = results.return();
+    const results = subscribe(schema, query);
+    const end = results.return();
 
-//         const r = end.then(res => {
-//             expect(returnSpy).toBeCalled();
-//         });
+    const r = end.then(res => {
+      expect(returnSpy).toBeCalled();
+    });
 
-//         pubsub.publish(FIRST_EVENT, {});
+    pubsub.publish(FIRST_EVENT, {});
 
-//         return r;
-//     });
-// });
-
+    return r;
+  });
+});
--- src/test/integration-tests.ts
+++ src/test/integration-tests.ts
@@ -1,14 +1,7 @@
-import * as chai from 'chai';
-import * as chaiAsPromised from 'chai-as-promised';
 import { mock } from 'simple-mock';
 import { ConsoleLogger } from '@cdm-logger/server';
 import { IRabbitMqConnectionConfig } from 'rabbitmq-pub-sub';
-import {
-  parse,
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-} from 'graphql';
+import { parse, GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
 import { isAsyncIterable } from 'iterall';
 import { subscribe } from 'graphql/subscription';
 import { withFilter } from 'graphql-subscriptions';
@@ -16,9 +9,6 @@ import { withFilter } from 'graphql-subscriptions';
 import { AmqpPubSub } from '../amqp-pubsub';
 
 const logger = ConsoleLogger.create('integration-test', { level: 'trace' });
-
-chai.use(chaiAsPromised);
-const expect = chai.expect;
 
 const FIRST_EVENT = 'FIRST_EVENT';
 
@@ -29,11 +19,11 @@ function buildSchema(iterator) {
       fields: {
         testString: {
           type: GraphQLString,
-          resolve: function (_, args) {
+          resolve: function(_, args) {
             return 'works';
-          },
-        },
-      },
+          }
+        }
+      }
     }),
     subscription: new GraphQLObjectType({
       name: 'Subscription',
@@ -43,14 +33,14 @@ function buildSchema(iterator) {
           subscribe: withFilter(() => iterator, () => true),
           resolve: root => {
             return 'FIRST_EVENT';
-          },
-        },
-      },
-    }),
+          }
+        }
+      }
+    })
   });
 }
 
-describe('PubSubAsyncIterator', function () {
+describe('PubSubAsyncIterator', function() {
   const query = parse(`
     subscription S1 {
       testSubscription
@@ -66,25 +56,24 @@ describe('PubSubAsyncIterator', function () {
 
   it('should allow subscriptions', () => {
     // tslint:disable-next-line:no-unused-expression
-    expect(isAsyncIterable(results)).to.be.true;
+    expect(isAsyncIterable(results)).toBe(true);
 
     const r = results.next();
     pubsub.publish(FIRST_EVENT, {});
 
     r.then(res => {
-      expect(res.value.data.testSubscription).to.equal('FIRST_EVENT');
+      expect(res.value.data.testSubscription).toBe('FIRST_EVENT');
     });
   });
 
   it('should clear event handlers', () => {
     // tslint:disable-next-line:no-unused-expression
-    expect(isAsyncIterable(results)).to.be.true;
+    expect(isAsyncIterable(results)).toBe(true);
 
     pubsub.publish(FIRST_EVENT, {});
 
-    results.return()
-      .then(res => {
-        expect(returnSpy.callCount).to.be.gte(1);
-      });
+    results.return().then(res => {
+      expect(returnSpy.callCount).toBeGreaterThanOrEqual(1);
+    });
   });
 });
